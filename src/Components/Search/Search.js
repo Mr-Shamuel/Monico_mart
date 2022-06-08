900b1facc72245270c1230934f5e7032662943c9
Shamuel <shamuelsam300@gmail.com>
2022-06-08 23:50:21 +0600

    remove unnecessery

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -6,7 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+ 
  
 const Search = () => {
     const [query, setQuery] = useState('');
